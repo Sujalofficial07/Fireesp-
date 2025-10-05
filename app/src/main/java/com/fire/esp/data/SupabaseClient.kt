41c243407b8d6992a113eaeb8e63a24f640ca9e0
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-10-05 13:38:31 +0530

    Update SupabaseClient.kt

--- app/src/main/java/com/fire/esp/data/SupabaseClient.kt
+++ app/src/main/java/com/fire/esp/data/SupabaseClient.kt
@@ -7,8 +7,8 @@ import io.github.jan.supabase.postgrest.Postgrest
 
 object SupabaseClientProvider {
 
-    private const val SUPABASE_URL = "https://YOUR-PROJECT-URL.supabase.co"
-    private const val SUPABASE_ANON_KEY = "YOUR-ANON-KEY"
+    private const val SUPABASE_URL = "https://rqjsgmxnzqemjztvdomr.supabase.co"
+    private const val SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxanNnbXhuenFlbWp6dHZkb21yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk1NjA1MzUsImV4cCI6MjA3NTEzNjUzNX0.xL5qWoqgwRdsrPTi3CRU-RGt3xfVmMMjwA99t8YyOU0"
 
     val client: SupabaseClient by lazy {
         createSupabaseClient(
