e6cd4cb25f909815b3e87823ee488621b2d0c965
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-10-05 17:09:05 +0530

    Update HomeActivity.kt

--- app/src/main/java/com/fire/esp/activities/HomeActivity.kt
+++ app/src/main/java/com/fire/esp/activities/HomeActivity.kt
@@ -2,40 +2,22 @@ package com.fire.esp.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import com.fire.esp.R
-import com.fire.esp.databinding.ActivityHomeBinding
-import com.fire.esp.fragments.AdminFragment
-import com.fire.esp.fragments.LeaderboardFragment
-import com.fire.esp.fragments.ProfileFragment
-import com.fire.esp.fragments.TournamentFragment
+import com.fire.esp.databinding.ActivityProfileBinding
+import com.fire.esp.utils.SupabaseClientManager
 
-class HomeActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityHomeBinding
+    private lateinit var binding: ActivityProfileBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityHomeBinding.inflate(layoutInflater)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Load default fragment
-        openFragment(TournamentFragment())
+        val currentUser = SupabaseClientManager.getCurrentUser()
 
-        binding.bottomNavigation.setOnItemSelectedListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.menuTournaments -> openFragment(TournamentFragment())
-                R.id.menuLeaderboard -> openFragment(LeaderboardFragment())
-                R.id.menuProfile -> openFragment(ProfileFragment())
-                R.id.menuAdmin -> openFragment(AdminFragment())
-            }
-            true
-        }
-    }
-
-    private fun openFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.containerHome, fragment)
-            .commit()
+        binding.tvEmail.text = currentUser?.email ?: "No Email"
+        binding.tvUserId.text = currentUser?.id ?: "No ID"
+        binding.tvDisplayName.text = currentUser?.fullName ?: "Unknown"
     }
 }
