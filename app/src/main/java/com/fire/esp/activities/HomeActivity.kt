b6f890e562f24b2fbb913e08bae12a5e7a0fa934
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-10-05 11:18:28 +0530

    Update HomeActivity.kt

--- app/src/main/java/com/fire/esp/activities/HomeActivity.kt
+++ app/src/main/java/com/fire/esp/activities/HomeActivity.kt
@@ -4,36 +4,38 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.fire.esp.R
+import com.fire.esp.databinding.ActivityHomeBinding
 import com.fire.esp.fragments.AdminFragment
 import com.fire.esp.fragments.LeaderboardFragment
 import com.fire.esp.fragments.ProfileFragment
 import com.fire.esp.fragments.TournamentFragment
 
 class HomeActivity : AppCompatActivity() {
-private lateinit var binding: ActivityHomeBinding
 
-override fun onCreate(savedInstanceState: Bundle?) {
-super.onCreate(savedInstanceState)
-binding = ActivityHomeBinding.inflate(layoutInflater)
-setContentView(binding.root)
+    private lateinit var binding: ActivityHomeBinding
 
-// Default fragment
-openFragment(TournamentFragment())
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-binding.bottomNavigation.setOnItemSelectedListener { menuItem ->
-when(menuItem.itemId){
-R.id.menuTournaments -> openFragment(TournamentFragment())
-R.id.menuLeaderboard -> openFragment(LeaderboardFragment())
-R.id.menuProfile -> openFragment(ProfileFragment())
-R.id.menuAdmin -> openFragment(AdminFragment())
-}
-true
-}
-}
+        // Load default fragment
+        openFragment(TournamentFragment())
 
-private fun openFragment(fragment: Fragment){
-supportFragmentManager.beginTransaction()
-.replace(R.id.containerHome, fragment)
-.commit()
-}
+        binding.bottomNavigation.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.menuTournaments -> openFragment(TournamentFragment())
+                R.id.menuLeaderboard -> openFragment(LeaderboardFragment())
+                R.id.menuProfile -> openFragment(ProfileFragment())
+                R.id.menuAdmin -> openFragment(AdminFragment())
+            }
+            true
+        }
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.containerHome, fragment)
+            .commit()
+    }
 }
