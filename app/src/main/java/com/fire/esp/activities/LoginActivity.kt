b66044582a7552d5f482b19ac7b97b0c213e3279
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-10-05 11:20:09 +0530

    Update LoginActivity.kt

--- app/src/main/java/com/fire/esp/activities/LoginActivity.kt
+++ app/src/main/java/com/fire/esp/activities/LoginActivity.kt
@@ -3,40 +3,36 @@ package com.fire.esp.activities
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.fire.esp.databinding.ActivityLoginBinding
 import com.fire.esp.utils.SupabaseClientManager
-import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
-import io.supabase.SupabaseClient
-import com.fire.esp.databinding.ActivityHomeBinding
-
 
 class LoginActivity : AppCompatActivity() {
 
-private lateinit var binding: ActivityLoginBinding
-private val scope = MainScope()
+    private lateinit var binding: ActivityLoginBinding
 
-override fun onCreate(savedInstanceState: Bundle?) {
-super.onCreate(savedInstanceState)
-binding = ActivityLoginBinding.inflate(layoutInflater)
-setContentView(binding.root)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-// Google login
-binding.btnGoogleLogin.setOnClickListener {
-scope.launch {
-val user = SupabaseClientManager.signInWithGoogle()
-if (user != null) openHome()
-}
-}
+        // Google login
+        binding.btnGoogleLogin.setOnClickListener {
+            lifecycleScope.launch {
+                val user = SupabaseClientManager.signInWithGoogle()
+                if (user != null) openHome()
+            }
+        }
 
-// Phone login
-binding.btnPhoneLogin.setOnClickListener {
-startActivity(Intent(this, PhoneLoginActivity::class.java))
-}
-}
+        // Phone login
+        binding.btnPhoneLogin.setOnClickListener {
+            startActivity(Intent(this, PhoneLoginActivity::class.java))
+        }
+    }
 
-private fun openHome() {
-startActivity(Intent(this, HomeActivity::class.java))
-finish()
-}
+    private fun openHome() {
+        startActivity(Intent(this, HomeActivity::class.java))
+        finish()
+    }
 }
