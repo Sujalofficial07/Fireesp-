45769502ff69180e9c235a84cfe4bd8ba3c486ba
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-10-05 11:17:50 +0530

    Update AdminActivity.kt

--- app/src/main/java/com/fire/esp/activities/AdminActivity.kt
+++ app/src/main/java/com/fire/esp/activities/AdminActivity.kt
@@ -5,10 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import com.fire.esp.R
 
 class AdminActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin)
 
-        // Admin can add/edit/delete tournaments
+        // TODO: Implement add/edit/delete tournaments functionality
     }
 }
