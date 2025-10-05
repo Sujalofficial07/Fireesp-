487f830316fcb57b2df11322e53ea81b274b4a30
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-10-05 13:41:51 +0530

    Update SupabaseConfig.kt

--- app/src/main/java/com/fire/esp/data/SupabaseConfig.kt
+++ app/src/main/java/com/fire/esp/data/SupabaseConfig.kt
@@ -5,9 +5,8 @@ import io.supabase.SupabaseClient
 object SupabaseClientProvider {
     val client: SupabaseClient by lazy {
         SupabaseClient(
-          supabase_url = "https://rqjsgmxnzqemjztvdomr.supabase.co"
-          supabase_anon_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxanNnbXhuenFlbWp6dHZkb21yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk1NjA1MzUsImV4cCI6MjA3NTEzNjUzNX0.xL5qWoqgwRdsrPTi3CRU-RGt3xfVmMMjwA99t8YyOU0"
-
+          supabaseUrl = SUPABASE_URL,
+          supabaseKey = SUPABASE_ANON_KEY
         )
     }
 }
