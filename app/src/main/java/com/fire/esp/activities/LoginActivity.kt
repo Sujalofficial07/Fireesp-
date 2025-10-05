195e250085aaff85a8d8a52d1f1cf65cab82760b
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-10-05 14:53:47 +0530

    Update LoginActivity.kt

--- app/src/main/java/com/fire/esp/activities/LoginActivity.kt
+++ app/src/main/java/com/fire/esp/activities/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.fire.esp.R
 import com.fire.esp.databinding.ActivityLoginBinding
 import com.fire.esp.utils.SupabaseClientManager
 import com.google.android.gms.auth.api.signin.GoogleSignIn
@@ -16,19 +17,17 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
     private lateinit var googleSignInClient: GoogleSignInClient
-    private val RC_SIGN_IN = 1001 // request code
+
+    companion object {
+        private const val RC_SIGN_IN = 9001
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Configure Google Sign-In
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.server_client_id)) // your server client ID
-            .requestEmail()
-            .build()
-        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        setupGoogleSignIn()
 
         // Google login
         binding.btnGoogleLogin.setOnClickListener {
@@ -42,26 +41,33 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
+    private fun setupGoogleSignIn() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+
         if (requestCode == RC_SIGN_IN) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
             try {
                 val account = task.getResult(ApiException::class.java)
-                val idToken = account.idToken
+                val idToken = account?.idToken
                 if (idToken != null) {
-                    loginWithSupabase(idToken)
+                    // Call Supabase sign-in with ID token
+                    lifecycleScope.launch {
+                        SupabaseClientManager.signInWithGoogle(idToken) { user ->
+                            if (user != null) openHome()
+                        }
+                    }
                 }
             } catch (e: ApiException) {
                 e.printStackTrace()
             }
-        }
-    }
-
-    private fun loginWithSupabase(idToken: String) {
-        lifecycleScope.launch {
-            val user = SupabaseClientManager.signInWithGoogle(idToken)
-            if (user != null) openHome()
         }
     }
 
