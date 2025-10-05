a1abba61c3a362a2e103af78aee312e0f6588909
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-10-05 11:34:24 +0530

    Update TournamentFragment.kt

--- app/src/main/java/com/fire/esp/fragments/TournamentFragment.kt
+++ app/src/main/java/com/fire/esp/fragments/TournamentFragment.kt
@@ -7,26 +7,25 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.fire.esp.adapters.TournamentAdapter
+import com.fire.esp.data.SupabaseClientManager
 import com.fire.esp.data.Tournament
 import com.fire.esp.databinding.ActivityTournamentBinding
-import io.supabase.SupabaseClient
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 
 class TournamentFragment : Fragment() {
+
     private lateinit var binding: ActivityTournamentBinding
     private lateinit var adapter: TournamentAdapter
     private val scope = MainScope()
-    private lateinit var supabase: SupabaseClient
     private var tournaments = mutableListOf<Tournament>()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         binding = ActivityTournamentBinding.inflate(inflater, container, false)
-
-        supabase = SupabaseClient(
-            supabaseUrl = "YOUR_SUPABASE_URL",
-            supabaseKey = "YOUR_SUPABASE_ANON_KEY"
-        )
 
         adapter = TournamentAdapter(tournaments)
         binding.recyclerTournaments.layoutManager = LinearLayoutManager(context)
@@ -38,12 +37,10 @@ class TournamentFragment : Fragment() {
 
     private fun loadTournaments() {
         scope.launch {
-            val response = supabase.from("tournaments").select("*").execute()
-            if(response.data != null){
-                tournaments.clear()
-                tournaments.addAll(response.data as List<Tournament>)
-                adapter.notifyDataSetChanged()
-            }
+            val fetchedTournaments: List<Tournament> = SupabaseClientManager.getAllTournaments()
+            tournaments.clear()
+            tournaments.addAll(fetchedTournaments)
+            adapter.notifyDataSetChanged()
         }
     }
 }
